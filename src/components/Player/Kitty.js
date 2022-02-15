1b7e6fee2cdc11a599a284c4318da6990b0b14f3
Michael Easter <codetojoy@gmail.com>
2022-02-15 08:30:55 -0400

    flexbox for kitty

--- src/components/Player/Kitty.js
+++ src/components/Player/Kitty.js
@@ -3,14 +3,20 @@ import Hand from "../Hand/Hand";
 
 import * as C from "../../C";
 
+import classes from "./Kitty.module.css";
+
 const Kitty = (props) => {
   const [prizeCard, ...kittyCards] = props.cards;
   return (
-    <div>
-      <p>Prize Card</p>
-      <Card value={prizeCard} doShow={true} isTransparent={props.isTransparent} className={C.CSS_AMBER_BUTTON} />
-      <p>Kitty</p>
-      <Hand cards={kittyCards} isTransparent={props.isTransparent} className={C.CSS_AMBER_BUTTON} />
+    <div className={classes.kitty}>
+      <div className={classes.kittyPrize}>
+        <p>Prize Card</p>
+        <Card value={prizeCard} doShow={true} isTransparent={props.isTransparent} className={C.CSS_AMBER_BUTTON} />
+      </div>
+      <div className={classes.kittyCards}>
+        <p>Kitty</p>
+        <Hand cards={kittyCards} isTransparent={props.isTransparent} className={C.CSS_AMBER_BUTTON} />
+      </div>
     </div>
   );
 };
